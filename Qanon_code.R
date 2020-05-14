de11473cb03eb5107f86d4776535806247bfbd58
EduardFabregat <45469244+EduardFabregat@users.noreply.github.com>
2020-05-14 12:12:36 -0400

    Update Qanon_code.R

--- Qanon_code.R
+++ Qanon_code.R
@@ -1,107 +1,222 @@
 ##############################################################
-######## Scrape Q drops
-options(stringsAsFactors = FALSE)
+######### Topic Modelling
 
-install.packages("rvest")
-library(rvest)
+install.packages("topicmodels")
 
-qdrops <- html("https://qmap.pub/?pg=19")
+library(doParallel)
 
-drops14 <- qdrops %>%
-  html_nodes(".text-muted.mr-2")%>%
-  html_text() %>% 
-  as.data.frame()
+mycores <- detectCores()
 
-drops <- rbind(drops1,drops2,drops3,drops4,drops5,drops6,drops7,drops8,drops9,drops10,drops11,
-               drops12,drops13,drops14,drops15)
+library(quanteda)
+# deleting links - notice that we added a "text" column for quanteda
+Qanon$text<-gsub("[h][t][t][p]\\S+","", Qanon$text)
+Qanon$text<-gsub("pic.twitter\\S+","", Qanon$text)
 
+#creem la variable index per a poder comparar l'lda i les dades originals
+#i aixi poder juntar les dues bases de dades
+#aixi despres podem trobar documents per a cada topic
+Qanon$index <- 1:nrow(Qanon)
 
-colnames(drops)[1] <- "drops"
-
-save(drops,file="Qdrops.Scraped.Rda")
-
-drops2 <- drops
-library(stringr)
-
-drop <- data.frame(temp=c(1))
-drop <- drop[,-1]
+### removing duplicate documents
+removed_df<-Qanon[duplicated(Qanon$text),]
+Q3 <- Qanon[!duplicated(Qanon$text),]
 
 
-for (date in 1:nrow(drops2)) {
-  temp <- as.data.frame(regmatches(drops2[date,],gregexpr("*?...............",drops2[date,])))
-  temp <- temp[1,]
-  temp <- as.data.frame(temp)
-  drop <- rbind(drop,temp)
-}
+# creating corpus object
+mycorpus <- corpus(Q3)
 
-Qdrops <- as.data.frame(mdy(drop$temp))
+# preprocessing
+stopwords_and_single<-c(stopwords("english"), "amp", LETTERS,letters)
+dfm_q <- dfm(mycorpus, tolower = TRUE, remove_punct = TRUE, 
+                   remove_numbers=TRUE, remove = stopwords_and_single, 
+                   stem = FALSE, remove_separators=TRUE)
 
-Qdrops$forsum <- 1
+docnames(dfm_q) <- dfm_q@docvars$index
 
-volume<-aggregate(x = Qdrops[,"forsum"],
-                  by = list(Qdrops$`mdy(drop$temp)`), FUN = "sum")
+dfm_q2 <- dfm_trim(dfm_q, max_docfreq = 0.95, min_docfreq = 0.01, 
+                       docfreq_type = "prop")
 
-openxlsx::write.xlsx(volume, "Qdrops.by.date.xlsx")
+dtm_lda <- convert(dfm_q2, to = "topicmodels")
 
-####################################################################
-########## Scrape all Q drops with a function
 
-url <-'https://qmap.pub/'
+full_data<-dtm_lda
 
-list_of_pages <- str_c(url, '?pg=', 1:67)
+n <- nrow(full_data)
 
-get_dates <- function(html){
-  html %>%
-    html_nodes(".text-muted.mr-2")%>%
-    html_text() %>%
-    str_trim() %>%                       
-    unlist() 
-}
+print(Sys.time())
+MainresultDF<-data.frame(k=c(1),perplexity=c(1),myalpha=c("x"))
+MainresultDF<-MainresultDF[-1,]
+#candidate_alpha<- c(0.01, 0.05, 0.1, 0.2, 0.5) # we choose variaty of alphas
+candidate_alpha<- c(0.5) # we choose variaty of alphas
+candidate_k <- c(seq(1,10)*10, 125, 150, 175, 200)
 
-get_drops <- function(html){
-  html %>%
-    html_nodes(".card-text")%>%
-    html_text() %>%
-    str_trim() %>%                       
-    unlist() 
-}
+save.image(file="searchk_10-200.RData")
+#m'he quedat aqui, fer el my cores i continuar amb el cross validation
 
-get_data_table <- function(html, company_name){
+library(doParallel)
+
+for (eachalpha in candidate_alpha) { 
+  print ("now running ALPHA:")
+  print (eachalpha)
+  print(Sys.time())
+  #----------------5-fold cross-validation, different numbers of topics----------------
+  cluster <- makeCluster(mycores) # leave one CPU spare...
+  registerDoParallel(cluster)
   
-  # Extract the Basic information from the HTML
-  dates_2 <- get_dates(html)
-  drops_2 <- get_drops(html)
+  clusterEvalQ(cluster, {
+    library(topicmodels)
+  })
   
-  # Combine into a tibble
-  combined_data <- tibble(date = dates_2,
-                          drops = drops_2) 
+  folds <- 5
+  splitfolds <- sample(1:folds, n, replace = TRUE)
+  #candidate_k <- c(2, 3, 4, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100) # candidates for how many topics
+  
+  #clusterExport(cluster, c("full_data", "burnin", "iter", "keep", "splitfolds", "folds", "candidate_k"))
+  clusterExport(cluster, c("full_data", "splitfolds", "folds", "candidate_k"))
+  
+  # we parallelize by the different number of topics.  A processor is allocated a value
+  # of k, and does the cross-validation serially.  This is because it is assumed there
+  # are more candidate values of k than there are cross-validation folds, hence it
+  # will be more efficient to parallelise
+  system.time({
+    results <- foreach(j = 1:length(candidate_k), .combine = rbind) %dopar%{
+      k <- candidate_k[j]
+      print(k)
+      results_1k <- matrix(0, nrow = folds, ncol = 2)
+      colnames(results_1k) <- c("k", "perplexity")
+      for(i in 1:folds){
+        train_set <- full_data[splitfolds != i , ]
+        valid_set <- full_data[splitfolds == i, ]
+        
+        fitted <- LDA(train_set, k = k, method = "Gibbs",
+                      #control = list(alpha=eachalpha/k,burnin = burnin, iter = iter, keep = keep) )
+                      control = list(verbose = 500,
+                                     alpha=eachalpha))
+        
+        #fitted <- LDA(train_set, k = k, method = "Gibbs")
+        
+        results_1k[i,] <- c(k, perplexity(fitted, newdata = valid_set))
+      }
+      return(results_1k)
+    }
+  })
+  stopCluster(cluster)
+  
+  results_df <- as.data.frame(results)
+  results_df$myalpha<-as.character(eachalpha)
+  MainresultDF<-rbind(MainresultDF,results_df)
+  print ("DONE!!!")
+  print(Sys.time())
 }
 
-get_data_from_url <- function(url, company_name){
-  html <- read_html(url)
-  get_data_table(html)
-}
+save(MainresultDF, file = "MainresultDF.02.Rda")
+save.image(file="Qanon.alpha.02.RData")
 
-scrape_write_table <- function(url, company_name){
-  
-  # Generate the target URLs
-  list_of_pages <- str_c(url, '?pg=', 1:67)
-  
-  # Apply the extraction and bind the individual results back into one table, 
-  # which is then written as a tsv file into the working directory
-  list_of_pages %>% 
-    # Apply to all URLs
-    map(get_data_from_url, company_name) %>%  
-    # Combine the tibbles into one tibble
-    bind_rows() %>%                           
-    # Write a tab-separated file
-    openxlsx::write.xlsx(str_c(company_name,'.xlsx'))     
-}
+MainresultDF$kalpha=paste0(as.character(MainresultDF$k),MainresultDF$myalpha) 
+ggplot(MainresultDF) +geom_boxplot(aes(x=k, y=perplexity, group=kalpha,color=myalpha))
 
-scrape_write_table(url,"QDropsAndDates")
+ggplot(MainresultDF) +
+  geom_boxplot(aes(x=k, y=perplexity, group=kalpha,color=myalpha))+
+  #geom_hline(yintercept=min(MainresultDF$perplexity[which(MainresultDF$myalpha==0.5)]),linetype = "dotted")+
+  geom_hline(yintercept=min(MainresultDF$perplexity[which(MainresultDF$myalpha==0.2)]),linetype = "dotted")+
+  geom_hline(yintercept=min(MainresultDF$perplexity[which(MainresultDF$myalpha==0.1)]),linetype = "dotted")+
+  geom_hline(yintercept=min(MainresultDF$perplexity[which(MainresultDF$myalpha==0.05)]),linetype = "dotted")+
+  geom_hline(yintercept=min(MainresultDF$perplexity[which(MainresultDF$myalpha==0.01)]),linetype = "dotted")
+
+#geom_line(aes(x=k, y=mean(perplexity),color=myalpha))
+#geom_smooth(se = TRUE, aes(x=k, y=perplexity,color=myalpha))
+
+alpha005 <- MainresultDF %>% 
+  filter(myalpha == 0.05)
+
+alpha001 <- MainresultDF %>% 
+  filter(myalpha == 0.01)
+
+
+alpha005 <- alpha005[order(alpha005$k),]
+
+MainresultDF<-MainresultDF[order(MainresultDF$k),]
+
+cars.spl <- with(alpha005, smooth.spline(k, perplexity, df = 3))
+with(cars, predict(cars.spl, x = alpha005$k, deriv = 2))
+
+plot(with(cars, predict(cars.spl, x = alpha005$k, deriv = 2)), type = "l")
+abline(v=70)
+
+runsdf<-data.frame(myk=c(60,70))
+
+mymodels<-list()
+
+cluster <- makeCluster(detectCores(logical = TRUE)) # leave one CPU spare...
+registerDoParallel(cluster)
+
+clusterEvalQ(cluster, {
+  library(topicmodels)
+})
+
+#clusterExport(cluster, c("full_data", "burnin", "iter", "keep", "splitfolds", "folds", "candidate_k"))
+clusterExport(cluster, c("full_data","runsdf"))
+
+system.time({
+  mymodels <- foreach(j = 1:nrow(runsdf)) %dopar%{
+    k_run <- runsdf[j,1]
+    #alpha_run<-runsdf[j,2]
+    fitted <- LDA(full_data, k = k_run, method = "Gibbs",
+                  control = list(alpha=0.05, seed=267348))
+                  #control = list(seed=3341) )
+  }
+})
+stopCluster(cluster)
 
 ###################################################################
 ############ Thematic Communities
+
+LDAfit<-mymodels[[2]]
+metadf<-data33
+meta_theta_df<-cbind(metadf,LDAfit@gamma)
+#the lda got rid of some tweets, I delete them since they don't have any theta values
+missing_docs<-setdiff(dfm_q2@Dimnames$docs,LDAfit@documents)
+dfm_q3<-dfm_q2[-which(dfm_q2@Dimnames$docs %in% missing_docs),]
+dfm_forsize<-data.frame(dfm_q3)
+dfm_forsize<-dfm_forsize[,-1]
+sizevect<-rowSums(dfm_forsize)
+meta_theta_df<-data.frame(size=sizevect,meta_theta_df)
+
+# now we prepare the removed duplicates dataset
+duplicate_df<-removed_df
+colnames(duplicate_df)<-paste0(colnames(duplicate_df),".1")
+
+# we cycle through all removed documents to add the missing theta values
+dflist<-list()
+for (i in (1:nrow(duplicate_df))) {
+  the_match<-match(duplicate_df$text.1[i],meta_theta_df$text)
+  newvect<-c(duplicate_df[i,],meta_theta_df[the_match,])
+  dflist[[i]]<-newvect
+}
+
+maintable<-data.frame(do.call(bind_rows,dflist))
+
+# we now delete the metadata from orginal matched document 
+#leaving only meta data for the actual document with the theta values and size 
+maintable<-data.frame(size=maintable$size,maintable[,-c((ncol(duplicate_df)+1):(ncol(duplicate_df)+ncol(metadf)+1))])
+colnames(maintable)<-gsub("\\.1","",colnames(maintable))
+
+#there are some NAs
+maintable2 <- maintable %>% 
+  filter(!size == "NA")
+
+meta_theta_df<-bind_rows(meta_theta_df,maintable2)
+
+meta_theta_df2 <- meta_theta_df
+
+tuisters <- meta_theta_df2 %>% 
+  count(screen_name)
+
+meta_theta_df3<-meta_theta_df2
+
+meta_theta_by_user_volume <- meta_theta_df3 %>% 
+  right_join(tuisters, by = "screen_name")
+
 
 meta_nort <- meta_theta_by_user_volume[meta_theta_by_user_volume$is_retweet != TRUE, ]
 
@@ -478,6 +593,109 @@ communities <- ggplot(q, aes(x=date)) +
   labs(title="Frequency of Tweets by Community")
 
 grid.arrange(drops, tweets, communities, nrow = 3)
+
+
+##############################################################
+######## Scrape Q drops
+options(stringsAsFactors = FALSE)
+
+install.packages("rvest")
+library(rvest)
+
+qdrops <- html("https://qmap.pub/?pg=19")
+
+drops14 <- qdrops %>%
+  html_nodes(".text-muted.mr-2")%>%
+  html_text() %>% 
+  as.data.frame()
+
+drops <- rbind(drops1,drops2,drops3,drops4,drops5,drops6,drops7,drops8,drops9,drops10,drops11,
+               drops12,drops13,drops14,drops15)
+
+
+colnames(drops)[1] <- "drops"
+
+save(drops,file="Qdrops.Scraped.Rda")
+
+drops2 <- drops
+library(stringr)
+
+drop <- data.frame(temp=c(1))
+drop <- drop[,-1]
+
+
+for (date in 1:nrow(drops2)) {
+  temp <- as.data.frame(regmatches(drops2[date,],gregexpr("*?...............",drops2[date,])))
+  temp <- temp[1,]
+  temp <- as.data.frame(temp)
+  drop <- rbind(drop,temp)
+}
+
+Qdrops <- as.data.frame(mdy(drop$temp))
+
+Qdrops$forsum <- 1
+
+volume<-aggregate(x = Qdrops[,"forsum"],
+                  by = list(Qdrops$`mdy(drop$temp)`), FUN = "sum")
+
+openxlsx::write.xlsx(volume, "Qdrops.by.date.xlsx")
+
+####################################################################
+########## Scrape all Q drops with a function
+
+url <-'https://qmap.pub/'
+
+list_of_pages <- str_c(url, '?pg=', 1:67)
+
+get_dates <- function(html){
+  html %>%
+    html_nodes(".text-muted.mr-2")%>%
+    html_text() %>%
+    str_trim() %>%                       
+    unlist() 
+}
+
+get_drops <- function(html){
+  html %>%
+    html_nodes(".card-text")%>%
+    html_text() %>%
+    str_trim() %>%                       
+    unlist() 
+}
+
+get_data_table <- function(html, company_name){
+  
+  # Extract the Basic information from the HTML
+  dates_2 <- get_dates(html)
+  drops_2 <- get_drops(html)
+  
+  # Combine into a tibble
+  combined_data <- tibble(date = dates_2,
+                          drops = drops_2) 
+}
+
+get_data_from_url <- function(url, company_name){
+  html <- read_html(url)
+  get_data_table(html)
+}
+
+scrape_write_table <- function(url, company_name){
+  
+  # Generate the target URLs
+  list_of_pages <- str_c(url, '?pg=', 1:67)
+  
+  # Apply the extraction and bind the individual results back into one table, 
+  # which is then written as a tsv file into the working directory
+  list_of_pages %>% 
+    # Apply to all URLs
+    map(get_data_from_url, company_name) %>%  
+    # Combine the tibbles into one tibble
+    bind_rows() %>%                           
+    # Write a tab-separated file
+    openxlsx::write.xlsx(str_c(company_name,'.xlsx'))     
+}
+
+scrape_write_table(url,"QDropsAndDates")
 
 ########################################################
 #######Time Series Analysis Tweets Frequency and Q Drops
